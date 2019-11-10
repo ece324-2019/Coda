fe3127dda7044f4987776da5f1f48c8937ae14ad
Lisa Li <>
2019-11-10 17:59:10 -0500

    added training loop and model

---
+++ main.py
@@ -0,0 +1,154 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+from model import ConvNN
+from model import MultiLP
+from model import MusicDataset
+
+import argparse
+from time import time
+import ipdb
+
+instruments_list = ["cel", "cla", "flu", "gac", "gel", "org", "pia", "sax", "tru", "vio", "voi"]
+
+def load_model(args):
+        # model = ConvNN()
+        model = MultiLP(len(instruments_list))
+        loss_func = torch.nn.MSELoss()
+        optimizer = torch.optim.SGD(model.parameters(),lr=args.lr)
+ 
+        return model, loss_func, optimizer
+
+def load_data(batch_size):
+        data = pd.read_csv('data/Part1.csv',names=["normalized", "instruments"])
+        # print(data.head())
+        # print("shape: ", data.shape)
+        # print(data["instruments"].value_counts())
+
+        labels = data["instruments"]
+        music_train = data["normalized"].values
+        ipdb.set_trace()
+        # Encode instruments
+        oneh_encoder = OneHotEncoder(categories="auto")
+       
+        # print(labels["instruments"])
+        label_oneh = oneh_encoder.fit_transform(labels.values.reshape(-1, 1)).toarray()
+        # print(label_oneh)
+        
+        train_data = MusicDataset(music_train, label_oneh)
+        train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+
+        return train_loader
+
+def main(args):
+        
+        model, loss_func, optimizer = load_model(args)
+        train_loader = load_data(args.batch_size)
+
+        running_loss = []
+        running_accuracy = []
+        # running_valid_loss = []
+        running_valid_accuracy = []
+        nRec = []
+
+        start = time()
+
+        for epoch in range(args.epochs):
+                train_acc = 0.0
+                train_loss = 0.0
+                total_count = 0.0
+
+                # ipdb.set_trace()
+
+                for j, data in enumerate(train_loader):
+                        feat, labels = data
+                
+                        optimizer.zero_grad()
+                        predict = model(feat).float()
+                
+                        loss = loss_func(predict.squeeze(), labels.float())
+                        loss.backward()
+                        optimizer.step()
+
+                        # _, predicted = torch.max(predict.data, 1)
+                        # trainAcc = (labels.nonzero()[:,1] == predicted).sum().item() /batch_size
+
+                        train_acc += int(((predict > 0.5).squeeze().float() == labels).sum())
+                        total_count += args.batch_size
+                        train_loss += loss.item()
+            
+                running_accuracy.append(train_acc/total_count)
+                running_loss.append(train_loss/float(i+1)) 
+
+                if epoch % args.eval_every == args.eval_every-1:
+                        # print("Epoch: %d | Training accuracy: %f | Training loss: %f | Val accuracy: %f | Val loss: %f"
+                        # % (epoch, running_accuracy[-1], running_loss[-1], running_valid_accuracy[-1], running_valid_loss[-1]))
+                        print("Epoch: %d | Training accuracy: %f | Training loss: %f"
+                        % (epoch+1, running_accuracy[-1], running_loss[-1]))
+
+        end = time()
+        print("====FINAL VALUES====")
+        # print("Test accuracy: %f | Test loss: %f" % (test_accuracy, test_loss))  
+        print("Training acc: %f | Valid acc: %f | Time: %f" % (max(running_accuracy), max(running_valid_accuracy), end - start))
+        # print("Training loss: %f | Valid loss: %f " % (min(running_loss), min(running_valid_loss)))
+        # print("overfit acc: %f | overfit loss: %f" % (max(overfit_accuracy), min(overfit_loss)))
+
+        fig = plt.figure()
+        ax = plt.subplot(1, 2, 1)
+        ax.plot(nRec, running_loss, label='Training')
+        # ax.plot(nRec,running_valid_loss, label='Validation')
+        plt.title('Training Loss vs. epoch')
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        ax.legend()
+
+        bx = plt.subplot(1, 2, 2)
+        bx.plot(nRec, running_accuracy, label='Training')
+        # bx.plot(nRec,running_valid_accuracy, label='Validation')
+        plt.title('Training Accuracy vs. epoch')
+        plt.xlabel("Epoch")
+        plt.ylabel("Accuracy")
+        bx.legend()
+        plt.show()
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--batch-size', type=int, default=64)
+        parser.add_argument('--lr', type=float, default=0.001)
+        parser.add_argument('--epochs', type=int, default=100)
+        parser.add_argument('--eval_every', type=int, default=10)
+
+        args = parser.parse_args()
+
+        main(args)
+
+# Remove instrument column and store in new variable
+# instrument_oneh = data["instruments"]
+# instrument_oneh = instrument_oneh.to_numpy()
+# data = data.drop(columns="instruments")
+
+# one_hot = []
+# one_hot.append(oneh_encoder.fit_transform(data["instruments"].values.reshape(-1, 1)).toarray())
+# one_hot = np.concatenate(one_hot, axis=1)
+# print("one hot", one_hot)
+# print("one hot shape", one_hot.shape)
+
+# oneh_df = pd.DataFrame(one_hot) 
+# data = data.drop(columns="instruments")
+# data = pd.concat((data, oneh_df), axis=1)
+
+
+
+
+
+
---
+++ model.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+
+
+neurons = 1000
+
+class MultiLP(nn.Module):
+
+    def __init__(self, input_size):
+        super(MultiLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, neurons)
+        self.fc2 = nn.Linear(neurons, neurons)
+        self.fc3 = nn.Linear(neurons, 13)
+
+    def forward(self, features):
+        x = F.relu(self.fc1(features.float()))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return x
+        
+
+class ConvNN(nn.Module):
+    def __init__(self):
+        super(ConvNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 4, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(4, 8, 5)
+
+        self.fc1 = nn.Linear(8*11*11, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+       
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 8*11*11)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # x = F.softmax(x, dim=1)
+        return x
+
+
+
+class MusicDataset(data.Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        # return [self.X[index, :], self.y[index]]
+        return self.X[index], self.y[index]
