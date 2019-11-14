cab2f3526f1ff1a42cc1bb0780c221244d5d6a21
mengyu-yang <my.yang@mail.utoronto.ca>
2019-11-13 22:42:43 -0500

    Added main_CNN

--- model.py
+++ model.py
@@ -24,19 +24,20 @@ class MultiLP(nn.Module):
 class ConvNN(nn.Module):
     def __init__(self):
         super(ConvNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 4, 5)
+        self.conv1 = nn.Conv2d(1, 3, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(4, 8, 5)
+        self.conv2 = nn.Conv2d(3, 8, 5)
 
-        self.fc1 = nn.Linear(8*11*11, 120)
+        self.fc1 = nn.Linear(8*253*29, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 11)
        
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 8*11*11)
+        print(x.shape)
+        x = x.view(-1, 8*253*29)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
