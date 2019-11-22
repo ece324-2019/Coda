71680359cc688376468932aa835343f64e32b0e1
mengyu-yang <my.yang@mail.utoronto.ca>
2019-11-22 18:24:22 -0500

    Added validation

--- transfer_learning.py
+++ transfer_learning.py
@@ -26,7 +26,6 @@ from model import MusicDataset
 import argparse
 from time import time
 
-
 # data_transforms = {
 #     'train': transforms.Compose([
 #         transforms.RandomResizedCrop(224),
@@ -58,132 +57,156 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # return train_loader, valid_loader, (music_data[0].shape[0] * music_data[0].shape[1])
 
 def main(args):
+    def evaluate(model, dataloader):
+        running_loss = 0.0
+        running_corrects = 0
+        with torch.no_grad():
+            for i, data in enumerate(dataloader):
+                inputs, labels = data
+                inputs = np.repeat(inputs.numpy()[..., np.newaxis], 3, -1)
+                inputs = torch.tensor(inputs).permute([0, 3, 1, 2])
+                if torch.cuda.is_available():
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-        def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-                since = time()
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
 
-                best_model_wts = copy.deepcopy(model.state_dict())
-                best_acc = 0.0
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            epoch_loss = running_loss / dataset_sizes
+            epoch_acc = running_corrects.double() / dataset_sizes
+        return epoch_loss, epoch_acc
 
-                for epoch in range(num_epochs):
-                        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-                        print('-' * 10)
+    def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+        since = time()
 
-                        # Each epoch has a training and validation phase
-                        # for phase in ['train', 'val']:
-                        #         if phase == 'train':
-                        #                 model.train()  # Set model to training mode
-                        #         else:
-                        #                 model.eval()   # Set model to evaluate mode
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_acc = 0.0
 
-                        running_loss = 0.0
-                        running_corrects = 0
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 10)
 
-                        # Iterate over data.
-                        for j, data in enumerate(train_loader):
-                                inputs, labels = data
-                                if torch.cuda.is_available():
-                                        inputs = inputs.to(device)
-                                        labels = labels.to(device)
+            # Each epoch has a training and validation phase
+            # for phase in ['train', 'val']:
+            #         if phase == 'train':
+            #                 model.train()  # Set model to training mode
+            #         else:
+            #                 model.eval()   # Set model to evaluate mode
 
-                                # zero the parameter gradients
-                                optimizer.zero_grad()
+            running_loss = 0.0
+            running_corrects = 0
 
-                                inputs = np.repeat(inputs.cpu().numpy()[..., np.newaxis], 3, -1)
-                                inputs = torch.tensor(inputs).to(device).permute([0, 3, 1, 2])
-                                # track history if only in train
-                                # with torch.set_grad_enabled(phase == 'train'):
+            # Iterate over data.
+            for j, data in enumerate(train_loader):
+                inputs, labels = data
+                if torch.cuda.is_available():
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-                                outputs = model(inputs)
-                                _, preds = torch.max(outputs, 1)
-                                loss = criterion(outputs, labels)
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-                                # backward + optimize only if in training phase
-                                # if phase == 'train':
-                                loss.backward()
-                                optimizer.step()
+                inputs = np.repeat(inputs.cpu().numpy()[..., np.newaxis], 3, -1)
+                inputs = torch.tensor(inputs).to(device).permute([0, 3, 1, 2])
+                # track history if only in train
+                # with torch.set_grad_enabled(phase == 'train'):
 
-                                # statistics
-                                running_loss += loss.item() * inputs.size(0)
-                                running_corrects += torch.sum(preds == labels.data)
-                        
-                        # if phase == 'train':
-                        scheduler.step()
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
 
-                        epoch_loss = running_loss / dataset_sizes
-                        epoch_acc = running_corrects.double() / dataset_sizes
+                # backward + optimize only if in training phase
+                # if phase == 'train':
+                loss.backward()
+                optimizer.step()
 
-                        print('Loss: {:.4f} Acc: {:.4f}'.format(
-                                epoch_loss, epoch_acc))
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
 
-                        # deep copy the model
-                        # if phase == 'val' and epoch_acc > best_acc:
-                        #         best_acc = epoch_acc
-                        #         best_model_wts = copy.deepcopy(model.state_dict())
+            # if phase == 'train':
+            scheduler.step()
 
-                        print()
+            epoch_loss = running_loss / dataset_sizes
+            epoch_acc = running_corrects.double() / dataset_sizes
 
-                time_elapsed = time() - since
-                print('Training complete in {:.0f}m {:.0f}s'.format(
-                        time_elapsed // 60, time_elapsed % 60))
-                print('Best val Acc: {:4f}'.format(best_acc))
+            model.eval()
+            val_loss, val_acc = evaluate(model, valid_loader)
+            model.train()
 
-                # load best model weights
-                model.load_state_dict(best_model_wts)
-                return model
+            print('Train Loss: {:.4f} Train Acc: {:.4f} Val Loss: {:.4f} Val Acc: {:.4f}'.format(
+                epoch_loss, epoch_acc, val_loss, val_acc))
 
-        data = pd.read_pickle('MFCC_harm.pkl')
-        data.columns = ["normalized", "instruments"]
-        label_encoder = LabelEncoder()
-        data['instruments'] = label_encoder.fit_transform(data['instruments'])
-        labels = data["instruments"].values
-        music_data = data["normalized"].values
+            # deep copy the model
+            # if phase == 'val' and epoch_acc > best_acc:
+            #         best_acc = epoch_acc
+            #         best_model_wts = copy.deepcopy(model.state_dict())
 
-        music_data = np.append(music_data[:3364], music_data[3365:])
-        labels = np.append(labels[:3364], labels[3365:])
+            print()
 
-        train_data, valid_data, train_labels, valid_labels = train_test_split(music_data, labels, test_size=0.2,
-                                                                                random_state=1)
-        oneh_encoder = OneHotEncoder(categories="auto", sparse=False)
-        train_set = MusicDataset(train_data, train_labels)
-        valid_set = MusicDataset(valid_data, valid_labels)
-        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
-        valid_loader = DataLoader(valid_set, batch_size=args.batch_size, shuffle=True)
-        print(train_loader)
+        time_elapsed = time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
 
-        # data_dir = 'data/'
-        # image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
-        #                                           data_transforms[x])
-        #                   for x in ['train', 'val']}
-        # dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
-        #                                              shuffle=True, num_workers=4)
-        #               for x in ['train', 'val']}
-        # dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-        # class_names = image_datasets['train'].classes
+        # load best model weights
+        model.load_state_dict(best_model_wts)
+        return model
 
+    data = pd.read_pickle('MFCC_harm.pkl')
+    data.columns = ["normalized", "instruments"]
+    label_encoder = LabelEncoder()
+    data['instruments'] = label_encoder.fit_transform(data['instruments'])
+    labels = data["instruments"].values
+    music_data = data["normalized"].values
 
-        dataset_sizes = len(train_data) 
+    music_data = np.append(music_data[:3364], music_data[3365:])
+    labels = np.append(labels[:3364], labels[3365:])
 
-        model_ft = models.resnet18(pretrained=True)
-        num_ftrs = model_ft.fc.in_features
+    train_data, valid_data, train_labels, valid_labels = train_test_split(music_data, labels, test_size=0.1,
+                                                                          random_state=1)
+    oneh_encoder = OneHotEncoder(categories="auto", sparse=False)
+    train_set = MusicDataset(train_data, train_labels)
+    valid_set = MusicDataset(valid_data, valid_labels)
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_set, batch_size=args.batch_size, shuffle=True)
+    print(train_loader)
 
+    # data_dir = 'data/'
+    # image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+    #                                           data_transforms[x])
+    #                   for x in ['train', 'val']}
+    # dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+    #                                              shuffle=True, num_workers=4)
+    #               for x in ['train', 'val']}
+    # dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    # class_names = image_datasets['train'].classes
 
-        # Here the size of each output sample is set to 2.
-        # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-        model_ft.fc = nn.Linear(num_ftrs, 11)
+    dataset_sizes = len(train_data)
 
-        model_ft = model_ft.to(device)
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
 
-        criterion = nn.CrossEntropyLoss()
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+    model_ft.fc = nn.Linear(num_ftrs, 11)
 
-        # Observe that all parameters are being optimized
-        optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    model_ft = model_ft.to(device)
 
-        # Decay LR by a factor of 0.1 every 7 epochs
-        exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    criterion = nn.CrossEntropyLoss()
 
-        model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                       num_epochs=25)
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                           num_epochs=25)
 
 
 if __name__ == '__main__':
