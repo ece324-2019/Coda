3bbf3b44e99cb4daefefb4545e46f0bf1388269e
lisa-sa-li <lisasiaili@gmail.com>
2019-11-24 13:25:27 -0500

    use all 3 test data pickels

--- multi_clean_5_class.py
+++ multi_clean_5_class.py
@@ -5,35 +5,37 @@ from sklearn.preprocessing import LabelEncoder
 import ipdb
 
 instruments_list = ["cel", "cla", "flu", "gac", "gel", "org", "pia", "sax", "tru", "vio", "voi"]
-# instruments_list.index("cel")
+# five_class = ["string", "wood", "key", "brass", "voi"]
 
-data = pd.read_pickle('test1.pkl')
+data1 = pd.read_pickle('test1.pkl')
+data2 = pd.read_pickle('test2.pkl')
+data3 = pd.read_pickle('test3.pkl')
+data = pd.concat([data1, data2, data3], ignore_index=True)
+
 data.columns = ["normalized", "instruments"]
 labels = data["instruments"].values
 music_data = data["normalized"].values
 
 # Remove bad data
-
+music_data = np.append(music_data[:1187], music_data[1188:])
+labels = np.append(labels[:1187], labels[1188:])
 music_data = np.append(music_data[:357], music_data[358:])
 labels = np.append(labels[:357], labels[358:])
+data = pd.DataFrame({'normalized': music_data, 'instruments': labels})
 
 # for i in range(music_data.shape[0]):
 # 	if not ((music_data[i] > -100).all() and (music_data[i] < 100).all()):
 # 		print('bad!, ', i)
 # print("yo")
 
-data = pd.DataFrame({'normalized': music_data, 'instruments': labels})
 
-# five_class = ["string", "wood", "key", "brass", "voi"]
-
-# Essentiall one hot encodes it
+# Essentially one hot encodes it
 for index, row in data.iterrows():
 	b = np.zeros(len(instruments_list))
-	# ipdb.set_trace()
 	for instrument in row['instruments']:
 		b[instruments_list.index(instrument)] = 1
 	row['instruments'] = b
 
 # Save
 data.to_pickle("11_multiclass.pkl")
-
+print("--Saved--")
