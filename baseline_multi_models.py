2ce87ee11d41585f52cc410eb32643faa42d8540
lisa-sa-li <lisasiaili@gmail.com>
2019-11-24 20:43:15 -0500

    changed to aug_mel dataset

--- baseline_multi_models.py
+++ baseline_multi_models.py
@@ -122,17 +122,18 @@ def main(args):
 		model.load_state_dict(best_model_wts)
 		return model
 
-	data = pd.read_pickle('11_multiclass.pkl')
+	data = pd.read_pickle('11_class.pkl')
 	# print(data['instruments'].value_counts())
 	labels = data["instruments"].values
 	music_data = data["normalized"].values
 
-	# oneh_encoder = OneHotEncoder(categories="auto")
-	# label_oneh = oneh_encoder.fit_transform(labels.reshape(-1, 1)).toarray()
+	oneh_encoder = OneHotEncoder(categories="auto")
+	labels = oneh_encoder.fit_transform(labels.reshape(-1, 1)).toarray()
 
 	music_data = np.stack(music_data).reshape(-1, 128*65) #65*128, 1025 * 65
 
 	train_data, valid_data, train_labels, valid_labels = train_test_split(music_data, labels, test_size=0.1, random_state=1)
+
 
 	train_set = MusicDataset(train_data, train_labels)
 	valid_set = MusicDataset(valid_data, valid_labels)
